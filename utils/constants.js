daf00972ea22f9427b52a65def9c19b5b9b42f5f
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-12-28 20:04:30 +0530

    Added urls

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,11 @@
 export const SEARCH_QUERY_URL =
   "https://www.zomato.com/webroutes/search/autoSuggest?addressId=0&entityId=3&entityType=city&locationType=&isOrderLocation=1&cityId=3&latitude=19.0176560000000000&longitude=72.8561780000000000&userDefinedLatitude=19.017656&userDefinedLongitude=72.856178&entityName=Mumbai&orderLocationName=Mumbai&cityName=Mumbai&countryId=1&countryName=India&displayTitle=Mumbai&o2Serviceable=true&placeId=77482&cellId=4316646509572521984&deliverySubzoneId=77482&placeType=DSZ&placeName=Mumbai&isO2City=true&fetchFromGoogle=false&fetchedFromCookie=false&&isO2OnlyCity=false&addressBlocker=0&&otherRestaurantsUrl=&q=";
+
+export const HEADER_LOGO =
+  "https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png";
+
+export const HOMEPAGE_BANNER =
+  "https://b.zmtcdn.com/web_assets/81f3ff974d82520780078ba1cfbd453a1583259680.png";
+
+export const HOMEPAGE_LOGO =
+  "https://b.zmtcdn.com/web_assets/8313a97515fcb0447d2d77c276532a511583262271.png";
